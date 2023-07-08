799d9851ca995213f1cd688cdab0bbedb3f575d3
danikova <kovacs.daniel.9509@gmail.com>
2023-07-08 12:27:26 +0200

    github-bot-register handler

--- backend/events/OnBeforeServe.go
+++ backend/events/OnBeforeServe.go
@@ -3,22 +3,109 @@ package events
 import (
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/forms"
+	"github.com/pocketbase/pocketbase/models"
 )
+
+type GithubMetaType struct {
+	Repository string `json:"repository"`
+	Run_id     string `json:"run_id"`
+}
+
+type GithubTaskType struct {
+	Output         string  `json:"output"`
+	Execution_time float32 `json:"execution_time"`
+}
+
+type GithubRequestBody struct {
+	Meta  GithubMetaType            `json:"meta"`
+	Tasks map[string]GithubTaskType `json:"tasks"`
+}
+
+type GithubResponseBody struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
 
 func OnBeforeServe(app *pocketbase.PocketBase) {
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
 
 		e.Router.AddRoute(echo.Route{
-			Method: http.MethodGet,
+			Method: http.MethodPost,
 			Path:   "/github-bot-register/",
 			Handler: func(c echo.Context) error {
-				return c.JSON(http.StatusOK, nil)
+				var reqBody GithubRequestBody
+				if err := c.Bind(&reqBody); err != nil {
+					return err
+				}
+
+				records, err := app.Dao().FindRecordsByExpr("run_results",
+					dbx.HashExp{"run_id": reqBody.Meta.Run_id},
+				)
+				if err != nil {
+					return err
+				}
+				if len(records) != 0 {
+					return echo.NewHTTPError(http.StatusNotAcceptable, "This run_id ("+reqBody.Meta.Run_id+") is already registered")
+				}
+
+				repo_url := "https://github.com/" + reqBody.Meta.Repository
+
+				workspace, err := app.Dao().FindFirstRecordByData("workspaces", "repo_url", repo_url)
+				if err != nil {
+					return err
+				}
+				if workspace == nil {
+					return echo.NewHTTPError(http.StatusNotAcceptable, "This repository ("+reqBody.Meta.Repository+") is not connected with any workspace")
+				}
+
+				collection, err := app.Dao().FindCollectionByNameOrId("run_results")
+				if err != nil {
+					return err
+				}
+
+				counter := 0
+				for task, data := range reqBody.Tasks {
+
+					item := models.NewRecord(collection)
+					form := forms.NewRecordUpsert(app, item)
+
+					form.LoadData(map[string]any{
+						"workspace":      workspace.Id,
+						"run_id":         reqBody.Meta.Run_id,
+						"task":           task,
+						"output":         data.Output[0:min(len(data.Output), 1000)],
+						"execution_time": data.Execution_time,
+					})
+
+					if err := form.Submit(); err != nil {
+						return err
+					}
+
+					counter += 1
+				}
+
+				resBody := &GithubResponseBody{
+					Code:    201,
+					Message: strconv.Itoa(counter) + " new record(s) generated",
+				}
+
+				return c.JSON(http.StatusOK, resBody)
 			},
 			Middlewares: []echo.MiddlewareFunc{
 				apis.ActivityLogger(app),
---
+++ backend/test/github-bot-register.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -X POST http://localhost:8090/github-bot-register/ \
+  -H 'Content-Type: application/json' \
+  -d '{"meta":{"repository":"danikova/example_project","run_id":"5493411022"},"tasks":{"task_1":{"execution_time":0.017,"output":"task_1"},"task_2":{"execution_time":0.016,"output":"task_2"},"task_3":{"execution_time":0.016,"output":"task_3"}}}'
