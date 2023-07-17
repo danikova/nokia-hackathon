21ac0cfc4d49d2036f85c751f6a203772a378e06
danikova <kovacs.daniel.9509@gmail.com>
2023-07-17 22:26:52 +0200

    set remote image urls

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,6 +12,14 @@ const nextConfig = {
       },
     ]
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
