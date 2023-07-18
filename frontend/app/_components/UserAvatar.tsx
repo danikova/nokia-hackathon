5929476f6f88dda01558cac5ccb0493ca46dea36
danikova <kovacs.daniel.9509@gmail.com>
2023-07-18 09:24:11 +0200

    info component added to avatar

--- frontend/app/_components/UserAvatar.tsx
+++ frontend/app/_components/UserAvatar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { Menu } from '@headlessui/react';
 import { setPBCookie, usePocketBase } from '../_lib/clientPocketbase';
 import Dropdown, { MenuItem, DropdownProps } from './inputs/Dropdown';
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { FaUserLarge } from 'react-icons/fa6';
 import { BiLogOut } from 'react-icons/bi';
 import { useRouter } from 'next/navigation';
@@ -20,27 +20,40 @@ export default function UserAvatar({ className, dropdownProps }: UserAvatarProps
   const [model, setModel] = useState<Record | Admin | null>(null);
 
   useEffect(() => {
-    pb.collection('users').authRefresh()
+    const cancelKey = "authrefresh"
+    pb.collection('users').authRefresh({ $cancelKey: cancelKey }).catch(() => { })
     pb.authStore.onChange((_, model) => {
       setModel(model)
     });
+    return () => {
+      pb.cancelRequest(cancelKey);
+    }
   }, [pb, setModel])
+
+  const InfoComp = useCallback(({ active }: { active: boolean }) =>
+    <div className={`flex min-w-full px-4 py-2 flex-col cursor-default ${active ? 'bg-primary-50 text-primary-700' : ''}`}>
+      <p>{model?.name || model?.username}</p>
+      <p className='text-[13px] -mt-1 opacity-75'>{model?.email}</p>
+    </div>, [model])
 
   const menuItems = useMemo<MenuItem[]>(
     () => [
+      {
+        key: 'info',
+        component: InfoComp
+      },
       {
         key: 'logout',
         component: LogoutButton,
       },
     ],
-    []
+    [InfoComp]
   );
 
   const menuButton = useMemo(() => {
-    console.log(model)
     if (model?.avatarUrl) {
       return (
-        <Menu.Button className="focus:ring-4 focus:outline-none focus:ring-blue-300">
+        <Menu.Button className="flex focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full">
           <Image height={40} width={40} className="w-8 h-8 rounded-full" src={model?.avatarUrl} alt={model?.email} />
         </Menu.Button>
       );
@@ -65,7 +78,7 @@ function LogoutButton({ active }: { active: boolean }) {
 
   return (
     <button
-      className={`flex min-w-full px-4 py-2 rounded-md ${active ? 'bg-primary-50 text-primary-700' : ''}`}
+      className={`flex min-w-full px-4 py-2 ${active ? 'bg-primary-50 text-primary-700' : ''}`}
       onClick={() => {
         pb.authStore.clear();
         setPBCookie(pb);
