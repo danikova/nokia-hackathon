1fa68f5469f9de5ff617eaf9e9ef29dbe07236d8
danikova <daniel.kovacs@nokia.com>
2023-07-20 19:57:22 +0200

    dashboard folder

--- frontend/app/(dashboard)/info/page.tsx
+++ frontend/app/(dashboard)/info/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function InfoHome() {
   const pb = getPB();
-  return <div className='px-16 py-8'>
+  return <div className='p-16 flex flex-col gap-8'>
     <Section imageSrc='/mici.jpeg'>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
     </Section>
@@ -33,7 +33,7 @@ export default function InfoHome() {
 }
 
 function Section({ imageSrc, children, reversed = false }: { imageSrc: string, children: ReactElement | string; reversed?: boolean }) {
-  return <div className={`flex items-end pt-8 gap-8 max-md:flex-col max-md:items-center ${reversed ? 'flex-row-reverse' : ''}`}>
+  return <div className={`flex items-end gap-8 max-md:flex-col max-md:items-center ${reversed ? 'flex-row-reverse' : ''}`}>
     <Image height={200} width={200} src={imageSrc} alt={"info section image"} className='' />
     <div>{children}</div>
   </div>
--- frontend/app/(dashboard)/results/page.tsx
+++ frontend/app/(dashboard)/results/page.tsx
@@ -1,42 +1,10 @@
 import { ListResult } from 'pocketbase';
 import { getPB } from '../../_lib/pocketbase';
-import Link from 'next/link';
-
-type RunResult = {
-  "id": string,
-  "collectionId": string,
-  "collectionName": string,
-  "created": string,
-  "updated": string,
-  "workspace": string,
-  "run_id": string,
-  "task": string,
-  "execution_time"?: number,
-  "output": string,
-  "is_success": boolean
-}
-
-type GroupedRunResult = {
-  [groupName: string]: RunResult[]
-}
-
-function getGroupedKeys(groupedRunResults: GroupedRunResult) {
-  return Object.keys(groupedRunResults).sort((a, b) => a.localeCompare(b))
-}
+import { RunResult, GroupedRunResult, getGroupedKeys, getGroupedRunResults } from './helpers';
 
 async function getRunResults() {
   const pb = getPB();
   return await pb.collection('run_results').getList<RunResult>(1, 50, { sort: '-created' });
-}
-
-function getGroupedRunResults(runResults: ListResult<RunResult>) {
-  const groups: GroupedRunResult = {};
-  for (const runResult of runResults.items) {
-    const groupName = runResult.task;
-    if (groupName in groups) groups[groupName].push(runResult);
-    else groups[groupName] = [runResult];
-  }
-  return groups;
 }
 
 export default async function ResultsHome() {
