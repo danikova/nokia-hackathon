e9d731391200a1f4a9813b346f8c01c1c8bd94ff
danikova <kovacs.daniel.9509@gmail.com>
2023-07-29 21:06:02 +0200

    better ux with focus lock

--- frontend/app/_components/inputs/ClientForm.tsx
+++ frontend/app/_components/inputs/ClientForm.tsx
@@ -1,11 +1,22 @@
 'use client'
 
-import { ReactElement } from "react";
+import { useOutsideClickObserver } from "@/app/_lib/helperHooks";
+import { ReactElement, useCallback, useRef, useState } from "react";
 import FocusLock from 'react-focus-lock';
 
 export default function ClientForm({ children, ...rest }: { children: ReactElement | ReactElement[], [k: string]: any }) {
-  return <FocusLock>
-    <form {...rest}>
+  const formRef = useRef<HTMLFormElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const onClick = useCallback(() => {
+    setIsFocused(true);
+  }, [setIsFocused]);
+  const onNotClick = useCallback(() => {
+    setIsFocused(false);
+  }, [setIsFocused]);
+  useOutsideClickObserver(formRef, onNotClick);
+
+  return <FocusLock disabled={!isFocused}>
+    <form {...rest} ref={formRef} onClick={onClick}>
       {children}
     </form>
   </FocusLock>
