62ccbe931b8c8358292d61d8f2a8f028858ea57b
danikova <kovacs.daniel.9509@gmail.com>
2023-08-07 10:50:30 +0200

    WIP: lexical added

--- frontend/app/(dashboard)/info/page.tsx
+++ frontend/app/(dashboard)/info/page.tsx
@@ -1,35 +1,40 @@
 import { ReactElement } from 'react';
 import { getPB } from '../../_lib/pocketbase';
 import Image from 'next/image';
+import Editor from './Editor';
 
 export default function InfoHome() {
   const pb = getPB();
   return <div className='m-16 max-md:m-8 flex flex-col gap-8'>
-    <Section imageSrc='/mici.jpeg'>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
-    </Section>
-    <Section imageSrc='/mici.jpeg' reversed>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
-    </Section>
-    <Section imageSrc='/mici.jpeg'>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
-    </Section>
-    <Section imageSrc='/mici.jpeg' reversed>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
-    </Section>
-    <Section imageSrc='/mici.jpeg'>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
-    </Section>
-    <Section imageSrc='/mici.jpeg' reversed>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
-    </Section>
-    <Section imageSrc='/mici.jpeg'>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
-    </Section>
-    <Section imageSrc='/mici.jpeg' reversed>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
-    </Section>
-  </div>;
+    <Editor />
+  </div>
+
+  // return <div className='m-16 max-md:m-8 flex flex-col gap-8'>
+  //   <Section imageSrc='/mici.jpeg'>
+  //     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
+  //   </Section>
+  //   <Section imageSrc='/mici.jpeg' reversed>
+  //     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
+  //   </Section>
+  //   <Section imageSrc='/mici.jpeg'>
+  //     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
+  //   </Section>
+  //   <Section imageSrc='/mici.jpeg' reversed>
+  //     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
+  //   </Section>
+  //   <Section imageSrc='/mici.jpeg'>
+  //     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
+  //   </Section>
+  //   <Section imageSrc='/mici.jpeg' reversed>
+  //     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
+  //   </Section>
+  //   <Section imageSrc='/mici.jpeg'>
+  //     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
+  //   </Section>
+  //   <Section imageSrc='/mici.jpeg' reversed>
+  //     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur turpis mauris, blandit a porta non, cursus eget velit. Vestibulum congue volutpat elementum. Vestibulum nec velit ultrices, commodo lorem ut, lacinia massa. Etiam a tincidunt nulla. Etiam non nibh ac mi tincidunt consectetur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur ultrices magna at malesuada tempus. Proin a eros cursus, volutpat erat ornare, sollicitudin enim. Nunc a dictum risus. Donec metus lacus, interdum non tincidunt vel, fermentum in est. Nullam pretium nunc odio, vitae varius arcu maximus sed.
+  //   </Section>
+  // </div>;
 }
 
 function Section({ imageSrc, children, reversed = false }: { imageSrc: string, children: ReactElement | string; reversed?: boolean }) {
