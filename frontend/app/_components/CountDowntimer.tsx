39b1f717abea2983ba21ba708780148e370111e3
danikova <kovacs.daniel.9509@gmail.com>
2023-08-14 09:12:34 +0200

    countdown timer fix

--- frontend/app/_components/CountDowntimer.tsx
+++ frontend/app/_components/CountDowntimer.tsx
@@ -23,38 +23,45 @@ function useGlobals() {
   return globals;
 }
 
-export default function CountDownTimer(props: any) {
-  const globals = useGlobals();
-
-  const endDate = useMemo(() => new Date(globals['event_end_date_time'] || '3000-01-01T00:00:00.000Z'), [globals]);
-  const [deltaTime, setDeltaTime] = useState(new Date(
+function useCountdownTime(endDate: Date) {
+  const [distance, setDeltaTime] = useState(
     endDate.getTime() - new Date().getTime()
-  ));
+  );
+  var days = useMemo(() => Math.floor(distance / (1000 * 60 * 60 * 24)), [distance]);
+  var hours = useMemo(() => Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)), [distance]);
+  var minutes = useMemo(() => Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)), [distance]);
+  var seconds = useMemo(() => Math.floor((distance % (1000 * 60)) / 1000), [distance]);
 
   useEffect(() => {
     const interval = setInterval(() => {
       const now = new Date();
-      setDeltaTime(new Date(
-        endDate.getTime() - now.getTime()
-      ));
+      setDeltaTime(endDate.getTime() - now.getTime());
     }, 1000);
     return () => clearInterval(interval);
   }, [endDate]);
+
+  return { days, hours, minutes, seconds };
+}
+
+export default function CountDownTimer(props: any) {
+  const globals = useGlobals();
+  const endDate = useMemo(() => new Date(globals['event_end_date_time'] || '3000-01-01T00:00:00.000Z'), [globals]);
+  const { days, hours, minutes, seconds } = useCountdownTime(endDate);
 
   return (
     <Tooltip>
       <TooltipTrigger>
         <span className={cn("countdown font-mono text-2xl opacity-40 hover:opacity-90 cursor-default", props.className)}>
-          <span style={{ "--value": deltaTime.getDay() } as React.CSSProperties}></span>:
-          <span style={{ "--value": deltaTime.getHours() } as React.CSSProperties}></span>:
-          <span style={{ "--value": deltaTime.getMinutes() } as React.CSSProperties}></span>:
-          <span style={{ "--value": deltaTime.getSeconds() } as React.CSSProperties}></span>
+          <span style={{ "--value": days } as React.CSSProperties}></span>:
+          <span style={{ "--value": hours } as React.CSSProperties}></span>:
+          <span style={{ "--value": minutes } as React.CSSProperties}></span>:
+          <span style={{ "--value": seconds } as React.CSSProperties}></span>
         </span>
       </TooltipTrigger>
       <TooltipContent>
         <div className="text-sm">
-          <div className="font-bold">Event ends</div>
-          <div>{dayjs(endDate).fromNow()} ({endDate.toLocaleDateString()} {endDate.toLocaleTimeString()})</div>
+          <div className="font-bold">Event ends {dayjs(endDate).fromNow()}</div>
+          <div>({endDate.toLocaleDateString()} {endDate.toLocaleTimeString()})</div>
         </div>
       </TooltipContent>
     </Tooltip>
--- frontend/app/index.ts
+++ frontend/app/index.ts
@@ -1,6 +1,7 @@
 'use client';
 
 import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
 import hljs from 'highlight.js/lib/core';
@@ -10,4 +11,23 @@ import 'highlight.js/styles/agate.css';
 
 hljs.registerLanguage('yaml', yaml);
 
-dayjs.extend(relativeTime);
+var thresholds = [
+  { l: 's', r: 1 },
+  { l: 'm', r: 1 },
+  { l: 'mm', r: 59, d: 'minute' },
+  { l: 'h', r: 1 },
+  { l: 'hh', r: 23, d: 'hour' },
+  { l: 'd', r: 1 },
+  { l: 'dd', r: 29, d: 'day' },
+  { l: 'M', r: 1 },
+  { l: 'MM', r: 11, d: 'month' },
+  { l: 'y', r: 1 },
+  { l: 'yy', d: 'year' },
+];
+var rounding = Math.ceil;
+
+dayjs.extend(duration);
+dayjs.extend(relativeTime, {
+  thresholds,
+  rounding,
+});
