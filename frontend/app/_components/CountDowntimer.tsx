e66babd912bda5e9e0db29834c167f34460ab0d6
danikova <kovacs.daniel.9509@gmail.com>
2023-08-16 17:58:26 +0200

    fix glitchy countdown timer

--- frontend/app/_components/CountDowntimer.tsx
+++ frontend/app/_components/CountDowntimer.tsx
@@ -29,25 +29,25 @@ function useCountdownTime(endDate: Date) {
 
 export default function CountDownTimer(props: any) {
   const globals = useGlobals();
-  const endDate = useMemo(() => new Date(globals['event_end_date_time'] || '3000-01-01T00:00:00.000Z'), [globals]);
+  const endDate = useMemo(() => new Date(globals['event_end_date_time']), [globals]);
   const { days, hours, minutes, seconds } = useCountdownTime(endDate);
 
   return (
     <Tooltip>
       <TooltipTrigger>
         <span className={cn("countdown font-mono text-2xl opacity-40 hover:opacity-90 cursor-default", props.className)}>
-          <span style={{ "--value": days } as React.CSSProperties}></span>:
-          <span style={{ "--value": hours } as React.CSSProperties}></span>:
-          <span style={{ "--value": minutes } as React.CSSProperties}></span>:
-          <span style={{ "--value": seconds } as React.CSSProperties}></span>
+          <span style={{ "--value": days || 0 } as React.CSSProperties}></span>:
+          <span style={{ "--value": hours || 0 } as React.CSSProperties}></span>:
+          <span style={{ "--value": minutes || 0 } as React.CSSProperties}></span>:
+          <span style={{ "--value": seconds || 0 } as React.CSSProperties}></span>
         </span>
       </TooltipTrigger>
-      <TooltipContent>
+      {endDate && <TooltipContent>
         <div className="text-sm">
           <div className="font-bold">Event ends <FromNow date={endDate} /></div>
           <div>({endDate.toLocaleDateString()} {endDate.toLocaleTimeString()})</div>
         </div>
-      </TooltipContent>
+      </TooltipContent>}
     </Tooltip>
   );
 }
