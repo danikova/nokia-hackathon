2b55a425c312a17cbc356410ae8b6f23fcb6b796
danikova <kovacs.daniel.9509@gmail.com>
2023-08-29 18:00:37 +0200

    auto resize on windows size change

--- frontend/components/ui/table/index.tsx
+++ frontend/components/ui/table/index.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { cn } from '@/lib/utils';
 import { AgGridReact, AgGridReactProps } from 'ag-grid-react';
 
 import './style.css';
+import { useDebounce, useWindowSize } from '@uidotdev/usehooks';
 
 interface FullPageAgGridReactProps<TData = any> extends AgGridReactProps<TData> {
   gridRef?: any;
@@ -13,6 +14,14 @@ interface FullPageAgGridReactProps<TData = any> extends AgGridReactProps<TData> 
 
 export function FullPageAgGridReact<TData = any>({ gridRef, className, ...props }: FullPageAgGridReactProps<TData>) {
   const { rowData } = props;
+  const size = useWindowSize();
+  const debouncedSize = useDebounce(size, 100);
+
+  useEffect(() => {
+    if (gridRef?.current) {
+      gridRef?.current?.api?.sizeColumnsToFit();
+    }
+  }, [debouncedSize]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <div className={cn('-mt-[var(--cm-titlebar-h)] h-full w-full box-border', className)} style={{ '--row-data-count': rowData?.length || 0 } as React.CSSProperties}>
