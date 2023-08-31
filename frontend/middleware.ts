b4cb8c88f61bcd3d4c978f7fe7e6fe7370383c0f
danikova <kovacs.daniel.9509@gmail.com>
2023-08-31 14:29:59 +0200

    fix scoreboard middleware issue

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getPB } from '@/lib/pocketbase';
 
-const staffRoutes = ['/scoreboard'];
+const staffRoutes = ['/ranking'];
 
 export async function middleware(request: NextRequest) {
   const pb = getPB(request);
