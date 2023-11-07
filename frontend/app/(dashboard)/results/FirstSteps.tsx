eaa9df98de501d57045fa3c67197686174d09a7e
danikova <kovacs.daniel.9509@gmail.com>
2023-11-07 17:44:36 +0100

    first step changes

--- frontend/app/(dashboard)/results/FirstSteps.tsx
+++ frontend/app/(dashboard)/results/FirstSteps.tsx
@@ -1,36 +1,99 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
 import Details from "./Details";
+import { useEffect, useState } from "react";
 import { navBarItems } from "@/lib/navBar";
 import BreadCrumb from "@/components/navigation/BreadCrumb";
 import { useGlobals, useIsWorkspaceBusy } from "@/lib/dataHooks";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+
+function Spacer() {
+  return <div className="w-1"></div>;
+}
 
 export default function FirstSteps() {
-  const globals = useGlobals()
+  const globals = useGlobals();
   const isLoading = useIsWorkspaceBusy();
+  const [showHoverCard, setShowHoverCard] = useState(false);
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setShowHoverCard(true);
+    }, 1500);
+    return () => clearTimeout(id);
+  }, []);
 
   return (
-    <div className='m-16 max-md:m-8'>
-      <BreadCrumb items={[navBarItems[1]]} />
-      <h2 className='text-2xl col-span-full pb-8'>
-        First steps
-      </h2>
-      <ul className="pb-8">
-        <li>You need to create a new public repository by using this <Link href={globals.primary_project || 'https://github.com'} className='text-primary hover:underline'>repo</Link> as a template</li>
-        <li className="flex items-center">You should create a new repository by clicking this button <Image alt='cat-404' src={'/use-this-template-btn.png'} width={150} height={30} /> and filling up the necessary details for repository creation</li>
-        <li>After creating the repository, you should go to <Link href='/settings' className='text-primary hover:underline'>/settings</Link> and set your repo in the <span className='font-semibold'>Used repo</span> section</li>
-        <li>Then you can use this repo to solve the task in your new repository</li>
-      </ul>
-      <Details />
-      <h2 className='text-2xl col-span-full pb-8'>
-        {
-          isLoading ?
-            'Please be patient while we are loading your first run results...' :
-            'There are no registered run results, you should commit your first change'
-        }
-      </h2>
-    </div>
+    <HoverCard open={showHoverCard}>
+      <div className="m-16 max-md:m-8">
+        <BreadCrumb items={[navBarItems[1]]} />
+        <h2 className="text-2xl col-span-full pb-8">First steps</h2>
+        <ul className="pb-8">
+          <li className="flex flex-wrap whitespace-nowrap">
+            You need to create a new public repository by using this
+            <Spacer />
+            <Link
+              href={globals.primary_project || "https://github.com"}
+              className="text-primary hover:underline"
+            >
+              repo
+            </Link>
+            <Spacer />
+            as a template
+          </li>
+          <li className="flex flex-wrap items-center whitespace-nowrap">
+            You should create a new repository by clicking this button
+            <Spacer />
+            <Link href={globals.primary_project || "https://github.com"}>
+              <Image
+                alt={"how-to-clone"}
+                className="min-w-[150px]"
+                src={"/use-this-template-btn.png"}
+                width={150}
+                height={30}
+              />
+              <Spacer />
+            </Link>
+            <Spacer />
+            and filling up the necessary details for repository creation,
+            <Spacer />
+            <span className="font-bold">
+              you must create public a repository
+            </span>
+          </li>
+          <li className="flex flex-wrap whitespace-nowrap">
+            After creating the repository, you should go to
+            <Spacer />
+            <Link href="/settings" className="text-primary hover:underline">
+              /settings
+            </Link>
+            <Spacer />
+            and set your repo in the
+            <Spacer />
+            <span className="font-semibold">Used repo</span> section
+          </li>
+          <li className="flex flex-wrap">
+            Then you can use this repo to solve the task in your new repository
+          </li>
+        </ul>
+        <Details />
+        <h2 className="text-2xl col-span-full pb-8">
+          {isLoading
+            ? "Please be patient while we are loading your first run results..."
+            : "There are no registered run results, you should commit your first change"}
+        </h2>
+        <HoverCardTrigger className="fixed bottom-[170px]" />
+        <HoverCardContent align="start">
+          You can find the Settings navigation menu on the lower left corner of
+          the page
+        </HoverCardContent>
+      </div>
+    </HoverCard>
   );
 }
