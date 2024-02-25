aa874a3ae7a32408fabf906696905fb0cbcd93ad
danikova <kovacs.daniel.9509@gmail.com>
2024-02-25 14:07:21 +0100

    OnRecordAfterUpdateRequest deleted

--- backend/src/events/init.go
+++ backend/src/events/init.go
@@ -3,13 +3,12 @@ package events
 import "github.com/pocketbase/pocketbase"
 
 func InitializeEvents(app *pocketbase.PocketBase) {
-    OnBeforeServe(app)
-    OnAfterBootstrap(app)
-    OnRecordAfterAuth(app)
-    OnRecordBeforeAuth(app)
-    OnRecordsListRequest(app)
-    OnRecordAfterCreateRequest(app)
-    OnRecordAfterUpdateRequest(app)
-    OnRecordBeforeUpdateRequest(app)
-    OnRecordBeforeCreateRequest(app)
+	OnBeforeServe(app)
+	OnAfterBootstrap(app)
+	OnRecordAfterAuth(app)
+	OnRecordBeforeAuth(app)
+	OnRecordsListRequest(app)
+	OnRecordAfterCreateRequest(app)
+	OnRecordBeforeUpdateRequest(app)
+	OnRecordBeforeCreateRequest(app)
 }
