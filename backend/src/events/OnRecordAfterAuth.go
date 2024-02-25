a0134c0c6600710aa984a33a60bf09c9aa3dbac3
danikova <kovacs.daniel.9509@gmail.com>
2024-02-25 13:50:37 +0100

    OnRecordAfterAuth refact

--- backend/src/events/OnRecordAfterAuth.go
+++ backend/src/events/OnRecordAfterAuth.go
@@ -2,6 +2,7 @@ package events
 
 import (
 	"hackathon-backend/src/tables"
+	"hackathon-backend/src/utils"
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
@@ -11,17 +12,19 @@ func OnRecordAfterAuth(app *pocketbase.PocketBase) {
 	app.OnRecordAfterAuthWithOAuth2Request().Add(func(e *core.RecordAuthWithOAuth2Event) error {
 		record, _ := app.Dao().FindFirstRecordByData(tables.WorkspacesCollectionName, tables.UserFieldKey, e.Record.Id)
 		if record == nil {
-			CreateWorkspaceForUser(app, &e.Record.Id)
+			utils.CreateWorkspaceForUser(app, &e.Record.Id)
 		}
 
-		if e.OAuth2User.Username != "" {
-			e.Record.Set("username", e.OAuth2User.Username)
+		fields := map[string]string{
+			"username":  e.OAuth2User.Username,
+			"name":      e.OAuth2User.Name,
+			"avatarUrl": e.OAuth2User.AvatarUrl,
 		}
-		if e.OAuth2User.Name != "" {
-			e.Record.Set("name", e.OAuth2User.Name)
-		}
-		if e.OAuth2User.AvatarUrl != "" {
-			e.Record.Set("avatarUrl", e.OAuth2User.AvatarUrl)
+
+		for key, value := range fields {
+			if value != "" {
+				e.Record.Set(key, value)
+			}
 		}
 
 		return app.Dao().SaveRecord(e.Record)
