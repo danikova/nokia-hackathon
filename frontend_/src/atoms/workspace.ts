5cc85113ed48adab171788ded25df190bcab3d1c
danikova <kovacs.daniel.9509@gmail.com>
2024-02-27 08:15:35 +0100

    new workspace atom

---
+++ frontend_/src/atoms/workspace.ts
@@ -0,0 +1,8 @@
+import { ReviewDialogProps } from "@/routes/_private/ranking/-components/reviewDialog/type";
+import { atom } from "jotai";
+
+export const workspaceDialogStateAtom = atom({
+  open: false,
+  props: null as null | ReviewDialogProps,
+});
+export const workspaceInfoOpenAtom = atom(false);
