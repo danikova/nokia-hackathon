31ff0d9498104df0df3c067dc0f72fcc5df51eff
danikova <kovacs.daniel.9509@gmail.com>
2024-02-27 16:04:15 +0100

    backend migration change

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 
+	_ "hackathon-backend/migrations"
 	"hackathon-backend/src/events"
 )
 
@@ -16,10 +19,9 @@ func main() {
 
 	events.InitializeEvents(app)
 
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
 	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
-		TemplateLang: migratecmd.TemplateLangJS,
-		Automigrate:  true,
-		Dir:          "pb_migrations",
+		Automigrate: isGoRun,
 	})
 
 	if err := app.Start(); err != nil {
