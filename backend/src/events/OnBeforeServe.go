9432bd49c9a6a7dfa915fd731c63e74d68986219
danikova <kovacs.daniel.9509@gmail.com>
2024-05-20 18:54:07 +0200

    custom_api internal server error fix

--- backend/src/events/OnBeforeServe.go
+++ backend/src/events/OnBeforeServe.go
@@ -3,10 +3,8 @@ package events
 import (
 	"hackathon-backend/src/crons"
 	"hackathon-backend/src/views"
-	"net/http"
 	"os"
 
-	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
@@ -14,14 +12,16 @@ import (
 
 func OnBeforeServe(app *pocketbase.PocketBase) {
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		e.Router.GET("/files/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
-		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./frontend"), false))
-		e.Router.HTTPErrorHandler = customHTTPErrorHandler
+		// e.Router.GET("/files/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		// e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./frontend"), true))
+		// e.Router.HTTPErrorHandler = customHTTPErrorHandler
 
 		views.AddGithubBotStarted(app, e)
 		views.AddGithubBotFinished(app, e)
 		views.AddGetRunResultsSumView(app, e)
 		views.AddGetRunStatisticsView(app, e)
+
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./frontend"), true))
 
 		crons.AddUpdateStaleRunTimers(app)
 
@@ -29,16 +29,16 @@ func OnBeforeServe(app *pocketbase.PocketBase) {
 	})
 }
 
-var defaultHTTPErrorHandler = echo.DefaultHTTPErrorHandler(false)
+// var defaultHTTPErrorHandler = echo.DefaultHTTPErrorHandler(false)
 
-func customHTTPErrorHandler(c echo.Context, err error) {
-	if he, ok := err.(*echo.HTTPError); ok {
-		if he.Code == http.StatusNotFound && c.Path() == "/*" {
-			c.File("./frontend/index.html")
-		} else {
-			defaultHTTPErrorHandler(c, err)
-		}
-	} else {
-		defaultHTTPErrorHandler(c, err)
-	}
-}
+// func customHTTPErrorHandler(c echo.Context, err error) {
+// 	if he, ok := err.(*echo.HTTPError); ok {
+// 		if he.Code == http.StatusNotFound && c.Path() == "/*" {
+// 			c.File("./frontend/index.html")
+// 		} else {
+// 			defaultHTTPErrorHandler(c, err)
+// 		}
+// 	} else {
+// 		defaultHTTPErrorHandler(c, err)
+// 	}
+// }
